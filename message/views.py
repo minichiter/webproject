5b6aa3a8df9033f35b5c88b2fe843f1d473b89ef
minichiter <minichit@gmail.com>
2025-11-04 03:42:45 +0330

    update: models, views, urls and templates

--- message/views.py
+++ message/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
-
-
+from django.views.generic import TemplateView, ListView
+from .models import Message
 
 # def message_views (request):
 #     return render(request,'home.html')
 
-class MessageView(TemplateView):
+class MessageView(ListView):
+    model = Message
     template_name =  'home.html'
